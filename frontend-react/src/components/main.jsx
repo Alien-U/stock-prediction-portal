8cc14d3e4f7032a9efb7522252fe214d042555f6
Utkarsh Dubey <dubeyutkarsh0004@gmail.com>
2025-12-07 16:44:15 +0530

    Auth functionalities

--- frontend-react/src/components/main.jsx
+++ frontend-react/src/components/main.jsx
@@ -7,7 +7,7 @@ const main = () => {
         <div className="p-5 text-center bg-light-dark rounded">
             <h1 className='text-light'>Stock Prediction Portal</h1>
             <p className="text-light lead">AI-powered stock research. Get insights in minutes. Invest smarter. Get real-time stock alerts and insights with our AI-powered stock research tool.Comprehensive insights, real-time analysis, and detailed stock research — all generated in under 2 minutes.</p>
-            <Button text='Login' class='btn-outline-info'/>
+            <Button text='Explore Now' class='btn-outline-info'/>
         </div>
     </div>
    </>
